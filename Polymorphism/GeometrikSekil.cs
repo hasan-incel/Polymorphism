4c25715365ad9a64b6d128a39fb3cd444f5b1b0c
Hasan İncel <h_incel@hotmail.com>
2024-08-11 04:05:53 +0300

    Add project files.

---
+++ Polymorphism/GeometrikSekil.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    // Base sınıf
+    public abstract class GeometrikSekil
+    {
+        // Propertyler
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        // Abstract methot
+        public abstract double AlanHesapla();
+    }
+    
+    //Kare sınıfı
+    public class Kare : GeometrikSekil
+    {
+        public Kare(double kenar)
+        {
+            Genislik = kenar;
+            Yukseklik = kenar;
+        }
+
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    // Dikdörtgen sınıfı
+    public class Dikdortgen : GeometrikSekil
+    {
+        public Dikdortgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    // Diküçgen sınıfı
+    public class DikUcgen : GeometrikSekil
+    {
+        public DikUcgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Polymorphism;
+
+// Kare nesnesi oluşturuluyor
+GeometrikSekil kare = new Kare(5);
+Console.WriteLine($"Kare'nin Alanı: {kare.AlanHesapla()}");
+
+// Dikdörtgen nesnesi oluşturuluyor
+GeometrikSekil dikdortgen = new Dikdortgen(5, 10);
+Console.WriteLine($"Dikdörtgen'in Alanı: {dikdortgen.AlanHesapla()}");
+
+// Dik Üçgen nesnesi oluşturuluyor
+GeometrikSekil dikUcgen = new DikUcgen(5, 10);
+Console.WriteLine($"Dik Üçgen'in Alanı: {dikUcgen.AlanHesapla()}");
